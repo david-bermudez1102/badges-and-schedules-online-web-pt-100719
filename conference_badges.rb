fc338eb697ffbe80c7c1c288b764bc9ece77bfbe
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 00:05:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,16 @@ def batch_badge_creator(attendees)
   badges = []
 
   attendees.each { |attendee|
-      badges << "Hello, my name is #{attendee}."
+    badges << "Hello, my name is #{attendee}."
   }
 
   badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  room = 1
+  attendees.each { |attendee|
+    room_assignments << "Hello, #{attendee}! You'll be assigned to room #{room}!"
+  }
 end
