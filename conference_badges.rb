bd9ed1f6454b01e01fa9949c83452627b53e842d
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 00:11:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,4 +25,15 @@ room_assignments
 end
 
 def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  room_assignments = assign_rooms(attendees)
+
+  badges.each { |badge|
+    puts badge
+  }
+
+  room_assignments.each { |room_assignment|
+    puts room_assignment
+  }
+  
 end
